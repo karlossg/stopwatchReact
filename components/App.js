ec185fb9e998efd92ff77c797def582b091a7420
karlossg <karol.gratus@gmail.com>
2017-12-14 23:30:54 +0100

    rendering formatted result

--- components/App.js
+++ components/App.js
@@ -1,11 +1,24 @@
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      minutes: '00',
-      seconds: '00',
-      miliseconds: '00'
-    };
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0
+    }
+  }
+
+  pad0(value) {
+    let result = value.toString()
+    if (result.length < 2) {
+      result = '0' + result
+    }
+    return result
+  }
+
+  format(times) {
+    console.log(times)
+    return `${this.pad0(times.minutes)}:${this.pad0(times.seconds)}:${this.pad0(Math.floor(times.miliseconds))}`
   }
 
   render() {
@@ -13,14 +26,12 @@ class App extends React.Component {
       <div>
         <button>start</button>
         <button>stop</button>
-        <div>
-          {this.state.minutes}:{this.state.seconds}:{this.state.miliseconds}
-        </div>
+        <div>{this.format(this.state)}</div>
         <button>reset</button>
         <button>save</button>
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
